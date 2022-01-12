de9ec3746fe204cda6df209cb6a731f2af953368
Eric Bartusch <ericbartusch8@gmail.com>
2022-01-11 22:50:57 -0600

    Cleanup and yellow relic name

--- BattleTowers/src/main/java/BattleTowers/events/NewBonfireEvent.java
+++ BattleTowers/src/main/java/BattleTowers/events/NewBonfireEvent.java
@@ -12,8 +12,10 @@ import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.vfx.cardManip.PurgeCardEffect;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import static BattleTowers.BattleTowers.makeID;
 
@@ -34,24 +36,24 @@ public class NewBonfireEvent extends PhasedEvent {
         Collections.shuffle(relics, new Random(AbstractDungeon.miscRng.randomLong()));
         this.relicChoice = relics.get(0);
 
+        String yellowRelicName = "#y" + Arrays.stream(relicChoice.name.split(" ")).collect(Collectors.joining(" #y"));
+
         //set up event
         registerPhase(0, new TextPhase(DESCRIPTIONS[0] + DESCRIPTIONS[1] + DESCRIPTIONS[2]).addOption(OPTIONS[0] + relicChoice.name + OPTIONS[1], (i)->transitionKey("Relic")).addOption(OPTIONS[2], (i)->transitionKey("Card")));
-        registerPhase("Brazil", new TextPhase(DESCRIPTIONS[1]).addOption(OPTIONS[2], (i)->transitionKey("Antarctica")).addOption(OPTIONS[4], (i)->transitionKey(1)));
-        registerPhase("Japan", new TextPhase(DESCRIPTIONS[2]).addOption(OPTIONS[0], (i)->transitionKey("Brazil")).addOption(OPTIONS[3], (i)->transitionKey("You can also use numbers")));
-        registerPhase("Antarctica", new TextPhase(DESCRIPTIONS[3]).addOption(OPTIONS[3], (i)->transitionKey("You can also use numbers")));
         registerPhase("Curse", new TextPhase(DESCRIPTIONS[6]).addOption(OPTIONS[4], (t)->this.openMap()));
         registerPhase("Basic", new TextPhase(DESCRIPTIONS[6]).addOption(OPTIONS[4], (t)->this.openMap()));
         registerPhase("Common", new TextPhase(DESCRIPTIONS[6]).addOption(OPTIONS[4], (t)->this.openMap()));
         registerPhase("Special", new TextPhase(DESCRIPTIONS[6]).addOption(OPTIONS[4], (t)->this.openMap()));
         registerPhase("Uncommon", new TextPhase(DESCRIPTIONS[6]).addOption(OPTIONS[4], (t)->this.openMap()));
         registerPhase("Rare", new TextPhase(DESCRIPTIONS[6]).addOption(OPTIONS[4], (t)->this.openMap()));
+        registerPhase("AfterRelic", new TextPhase(DESCRIPTIONS[4] + yellowRelicName + DESCRIPTIONS[5]).addOption(OPTIONS[4], (t)->this.openMap()));
 
-        registerPhase("Relic", new TextPhase(DESCRIPTIONS[7]).addOption(OPTIONS[5] + relicChoice.name, (i)->{
+        registerPhase("Relic", new TextPhase(DESCRIPTIONS[7]).addOption(OPTIONS[5] + yellowRelicName, (i)->{
             AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
             AbstractDungeon.player.loseRelic(relicChoice.relicId);
             AbstractDungeon.getCurrRoom().baseRareCardChance = 1000; //Always give rares
             AbstractDungeon.combatRewardScreen.open(DESCRIPTIONS[8]);
-            transitionKey("Curse");
+            transitionKey("AfterRelic");
         }));
 
         registerPhase("Card", new TextPhase(DESCRIPTIONS[3])
@@ -72,6 +74,7 @@ public class NewBonfireEvent extends PhasedEvent {
                             break;
                         case SPECIAL:
                             transitionKey("Special");
+                            AbstractDungeon.getCurrRoom().spawnRelicAndObtain((Settings.WIDTH / 2), (Settings.HEIGHT / 2), AbstractDungeon.returnRandomRelic(AbstractRelic.RelicTier.RARE));
                             break;
                         case UNCOMMON:
                             transitionKey("Uncommon");
@@ -89,10 +92,10 @@ public class NewBonfireEvent extends PhasedEvent {
                 }
             }
         }.addOption(OPTIONS[3], (i)->{
-            AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE; //If it's not over don't do this
+            AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
             AbstractDungeon.gridSelectScreen.open(AbstractDungeon.player.masterDeck.getPurgeableCards(), 1, OPTIONS[2], false, false, false, true);
         }));
 
-        transitionKey(0); //starting point
+        transitionKey(0);
     }
 }
