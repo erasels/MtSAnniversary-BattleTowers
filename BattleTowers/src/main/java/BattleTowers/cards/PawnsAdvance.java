3d0b410d4bc2b06fd798258375231f3cf59939c1
Vex Darkon <edanvirgo@gmail.com>
2022-01-11 19:36:45 -0700

    Otto cards

---
+++ BattleTowers/src/main/java/BattleTowers/cards/PawnsAdvance.java
@@ -0,0 +1,65 @@
+package BattleTowers.cards;
+
+
+import BattleTowers.BattleTowers;
+import BattleTowers.powers.PawnBuffPower;
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static BattleTowers.BattleTowers.makeID;
+
+
+public class PawnsAdvance extends CustomCard {
+    public static final String ID = makeID(PawnsAdvance.class.getSimpleName());
+    public static final String NAME;
+    public static final String DESCRIPTION;
+    public static final String IMG_PATH = BattleTowers.makeCardPath("PawnsAdvance.png");
+    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.SELF_AND_ENEMY;
+    private static final CardStrings cardStrings;
+    private static final int COST = 1;
+    public static String UPGRADED_DESCRIPTION;
+
+    static {
+        cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+        NAME = cardStrings.NAME;
+        DESCRIPTION = cardStrings.DESCRIPTION;
+        UPGRADED_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    }
+
+    public PawnsAdvance() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, CardColor.COLORLESS, RARITY, TARGET);
+        baseDamage = 4;
+        baseBlock = 4;
+        baseMagicNumber = magicNumber = 2;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, block));
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        addToBot(new ApplyPowerAction(p, p, new PawnBuffPower(magicNumber), magicNumber));
+    }
+
+    public AbstractCard makeCopy() {
+        return new PawnsAdvance();
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+        }
+    }
+}
+
+
