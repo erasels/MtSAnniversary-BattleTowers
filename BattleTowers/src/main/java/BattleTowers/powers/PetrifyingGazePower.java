08e7ae203c6d6612ea2e415838f131947728e2cc
modargo <modargo@gmail.com>
2022-01-17 17:27:14 -0500

    Fix Petrifying Gaze when DamageInfo has no owner, and make it apply to all types of damage

--- BattleTowers/src/main/java/BattleTowers/powers/PetrifyingGazePower.java
+++ BattleTowers/src/main/java/BattleTowers/powers/PetrifyingGazePower.java
@@ -28,10 +28,11 @@ public class PetrifyingGazePower extends AbstractPower {
 
     @Override
     public int onAttacked(DamageInfo info, int damageAmount) {
-        if (!info.owner.hasPower(AvertYourGazePower.POWER_ID)) {
-            if (damageAmount < this.owner.currentHealth && damageAmount > 0 && info.owner != null && info.type == DamageInfo.DamageType.NORMAL) {
-                this.trigger(info.owner);
-            }
+        if (info.owner != null
+                && !info.owner.hasPower(AvertYourGazePower.POWER_ID)
+                && damageAmount < this.owner.currentHealth
+                && damageAmount > 0) {
+            this.trigger(info.owner);
         }
 
         return damageAmount;
