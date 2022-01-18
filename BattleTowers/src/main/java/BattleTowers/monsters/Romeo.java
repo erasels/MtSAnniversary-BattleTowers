a6eac4323962ecac9dcf40405c661e6b2f972664
erasels <emanuelbedburdick@yahoo.de>
2022-01-18 11:41:29 +0100

    Added special event if Masked Bandits were killed to Romeo encounter

--- BattleTowers/src/main/java/BattleTowers/monsters/Romeo.java
+++ BattleTowers/src/main/java/BattleTowers/monsters/Romeo.java
@@ -3,6 +3,9 @@ package BattleTowers.monsters;
 import BattleTowers.powers.CounterPower;
 import BattleTowers.powers.GrievousWoundsPower;
 import BattleTowers.util.UC;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.esotericsoftware.spine.AnimationState;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
@@ -16,9 +19,14 @@ import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.events.city.MaskedBandits;
 import com.megacrit.cardcrawl.localization.MonsterStrings;
 import com.megacrit.cardcrawl.monsters.EnemyMoveInfo;
+import com.megacrit.cardcrawl.powers.IntangiblePlayerPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.vfx.UncommonPotionParticleEffect;
+
+import java.util.HashMap;
 
 import static BattleTowers.BattleTowers.makeID;
 import static BattleTowers.BattleTowers.makeMonsterPath;
@@ -47,8 +55,8 @@ public class Romeo extends AbstractBTMonster {
 
     private boolean hadTalk1 = false, hadTalk2 = false;
     private int turn = 1;
+    private boolean ded;
 
-    //TODO: Make transparent and give particle effect if kill bandits was chosen in Masked Bandits event
     public Romeo() {this(0f,0f);}
     public Romeo(final float x, final float y) {
         super(NAME, ID, 50, -10.0F, -7.0F, 180.0F, 285.0F, null, x, y);
@@ -68,6 +76,14 @@ public class Romeo extends AbstractBTMonster {
         addMove(SIDE_SLASH, Intent.ATTACK_DEFEND, MED_DMG);
         addMove(STAB, Intent.ATTACK, SMALL_DMG, AbstractDungeon.ascensionLevel>=17 ? 2 : 1, true);
         addMove(GRIEVOUS_WOUNDS, Intent.STRONG_DEBUFF);
+
+        if(CardCrawlGame.isInARun())
+            for(HashMap map : CardCrawlGame.metricData.event_choices) {
+                if(map.getOrDefault("event_name", "").equals(MaskedBandits.ID) && map.get("player_choice").equals("Fought Bandits")) {
+                    ded = true;
+                    break;
+                }
+            }
     }
 
     @Override
@@ -80,6 +96,9 @@ public class Romeo extends AbstractBTMonster {
     @Override
     public void usePreBattleAction() {
         UC.doPow(this, this, new CounterPower(this, 7, calcAscensionDamage(SMALL_DMG)), false);
+        if(ded) {
+            UC.doPow(this, this, new IntangiblePlayerPower(this, 1), false);
+        }
     }
 
     @Override
@@ -94,7 +113,7 @@ public class Romeo extends AbstractBTMonster {
         switch (nextMove) {
             case GRIEVOUS_WOUNDS:
                 if(!hadTalk1) {
-                    UC.atb(new TalkAction(this, DIALOG[0]));
+                    UC.atb(new TalkAction(this, DIALOG[ded?2:0]));
                     hadTalk1 = true;
                 }
                 UC.doPow(this, UC.p(), new GrievousWoundsPower(UC.p(), calcAscensionSpecial(MHP_DEC)), false);
@@ -148,5 +167,38 @@ public class Romeo extends AbstractBTMonster {
             state.setTimeScale(0.8F);
             state.addAnimation(0, "Idle", true, 0.0F);
         }
+    }
+
+    private Color backupTintCol;
+    private float particleTimer = 0f;
+    private static final float PARTICLE_COOLDOWN = 0.15f;
+    @Override
+    public void render(SpriteBatch sb) {
+        if(ded) {
+            backupTintCol = tint.color.cpy();
+            tint.color = oscillarator(tint.color);
+            particleTimer -= UC.gt();
+            if(!Settings.DISABLE_EFFECTS && particleTimer <= 0) {
+                AbstractDungeon.effectList.add(new UncommonPotionParticleEffect(MathUtils.random(hb.x, hb.x+hb.width), MathUtils.random(hb.y, hb.y+hb.height)));
+                particleTimer = PARTICLE_COOLDOWN;
+            }
+        }
+        super.render(sb);
+        if(ded) {
+            tint.color = backupTintCol;
+        }
+    }
+
+    private static float oscillatingTimer = 0.0f;
+    private static float oscillatingFader = 0.0f;
+    public static Color oscillarator(Color c) {
+        oscillatingFader += Gdx.graphics.getRawDeltaTime();
+        if (oscillatingFader > 0.66F) {
+            oscillatingFader = 0.66F;
+            oscillatingTimer += Gdx.graphics.getRawDeltaTime() * 1.5f;
+        }
+        Color col = c.cpy();
+        col.a = (0.5F + (MathUtils.cos(oscillatingTimer) + 1.0F) / 3.0F) * oscillatingFader;
+        return col;
     }
 }
