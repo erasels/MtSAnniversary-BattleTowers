5e98da3f767922aa9789a739ce298cbba1990d18
erasels <emanuelbedburdick@yahoo.de>
2022-01-19 09:24:43 +0100

    Reworked RainbowLouses to be mroe forgiving

--- BattleTowers/src/main/java/BattleTowers/monsters/RainbowLouse.java
+++ BattleTowers/src/main/java/BattleTowers/monsters/RainbowLouse.java
@@ -43,9 +43,9 @@ public class RainbowLouse extends AbstractBTMonster {
     private static final String REAR = "REAR";
 
     //Monster stats
-    private static final int MIN_HP = 16;
-    private static final int MAX_HP = 22;
-    private static final int STRUGGLE_DAMAGE = 2;
+    private static final int MIN_HP = 20;
+    private static final int MAX_HP = 26;
+    private static final int STRUGGLE_DAMAGE = 4;
     private static final int BASE_STRUGGLE_HITS = 2;
     private final int STR_AMOUNT = calcAscensionSpecial(1);
     private final int DEBUFF_AMOUNT = 1;
