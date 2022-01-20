b2098fa450cccb287cd69a32728a5bb3dea73011
lobbienjonsji <36815592+lobbienjonsji@users.noreply.github.com>
2022-01-20 17:45:59 +0100

    Why did the code optimization get unchecked!?!?

--- BattleTowers/src/main/java/BattleTowers/actions/CopyCatAction.java
+++ BattleTowers/src/main/java/BattleTowers/actions/CopyCatAction.java
@@ -3,28 +3,23 @@ package BattleTowers.actions;
 import BattleTowers.monsters.AspiringChampion;
 import BattleTowers.powers.CopyCatPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.utility.ShowCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.StrengthPower;
-import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 import com.megacrit.cardcrawl.vfx.MegaSpeechBubble;
 import com.megacrit.cardcrawl.vfx.cardManip.ShowCardBrieflyEffect;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CopyCatAction extends AbstractGameAction {
     AspiringChampion owner;
     String msg;
     String msgNoAttacks;
     private boolean used;
-    private float bubbleDuration;
     
     public CopyCatAction(AspiringChampion aspiringChampion, String msg, String msgNoAttacks)
     {
